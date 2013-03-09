9a24cfca5ef140f2f9b6d255068c9455b1329d8d
manu <manuel.will@insphare.de>
2013-03-09 14:39:33 +0100

    Add usage example

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ Cli-Options
 ===========
 
 Mini - PHP cli options framework
+
+Example:
+
+show help:		php example.php --help
+wrong usage:	php example.php
+in action: 		php example.php -h="172.1.2.3" -u="john" --debug --log="logfile.txt"
--- example.php
+++ example.php
@@ -1,5 +1,8 @@
 <?php
 
+include_once 'Options.php';
+include_once 'Option.php';
+
 $option = new Cli_Options(__FILE__);
 $option->addShortOption('h', Cli_Options::OPTION_TYPE_REQUIRED, 'HOST', 'The host. Example: 172.0.0.1');
 $option->addShortOption('p', Cli_Options::OPTION_TYPE_OPTIONAL, 'PORT', 'The Port.', 99);
@@ -11,13 +14,13 @@ $option->addLongOption('log', Cli_Options::OPTION_TYPE_OPTIONAL, 'Log', 'Logging
 
 $option->parse();
 
-// php optiontest.php -h="172.1.2.3" -u="john" --debug --log="logfile.txt"
+// php example.php -h="172.1.2.3" -u="john" --debug --log="logfile.txt"
 
 echo PHP_EOL;
-echo('h: '.$option->getOptionValue('h')) . PHP_EOL;
-echo('p: '.$option->getOptionValue('p')) . PHP_EOL;
-echo('u: '.$option->getOptionValue('u')) . PHP_EOL;
-echo('c: '.$option->getOptionValue('c'))  . PHP_EOL;
-echo('debug: '.$option->getOptionValue('debug'))  . PHP_EOL;
-echo('log: '.$option->getOptionValue('log')) . PHP_EOL;
+echo('h: ' . $option->getOptionValue('h')) . PHP_EOL;
+echo('p: ' . $option->getOptionValue('p')) . PHP_EOL;
+echo('u: ' . $option->getOptionValue('u')) . PHP_EOL;
+echo('c: ' . $option->getOptionValue('c')) . PHP_EOL;
+echo('debug: ' . $option->getOptionValue('debug')) . PHP_EOL;
+echo('log: ' . $option->getOptionValue('log')) . PHP_EOL;
 echo PHP_EOL;
